f164215783159ce1face1234584e7d16df5375d8
Mahesh-MSFT <maksh@microsoft.com>
2017-04-10 17:41:00 +0530

    First Commit

---
+++ AnalyticsDemoApp/src/AnalyticsDemoApp/Models/OutgoingResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnalyticsDemoApp.Models
+{
+    public class OutgoingResponse
+    {
+        public string NewCartItem { get; set; }
+
+        public Dictionary<string, string> CartItemsOfOthers { get; set; }
+    }
+}
